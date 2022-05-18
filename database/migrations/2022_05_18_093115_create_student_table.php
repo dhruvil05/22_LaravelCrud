3f9f3ce7c632aee0c8124288c666542122033600
Your Name <dhruvil.patel23117@gmail.com>
2022-05-18 18:35:17 +0530

    commit2

---
+++ database/migrations/2022_05_18_093115_create_student_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 100);
+            $table->date('dateOfBfirth');
+            $table->enum('gender',["M","F","O"]);
+            $table->string('country',100);
+            $table->string('favSport', 100);
+            $table->string('image', 100);
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
